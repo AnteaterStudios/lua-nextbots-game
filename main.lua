28773f4ad9ffa6fcd4d79fe3ce9fbac50f92d16c
Anteater Studios <anteaterstudios.games@gmail.com>
2022-11-26 14:31:50 -0300

    Finish developing multiple nextbots spawn
    - Obunga
    - Baller
    - Rock
    - Spongebob
    
    Also fix random position spawning and increased floor.
    
    :trollface:

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 -- constants
 local NB_SIZE = 2.5
+local RANDOM_POS_MAX = 50
+
 -- variables
 local sky
 local headset_pos
@@ -9,19 +11,19 @@ local headset_ay
 local headset_az
 
 local nextbots = {
-  obunga = {name = "obunga", image = "obunga.png", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil};
-  baller = {name = "baller", image = "baller.png", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil};
-  rock = {name = "rock", image = "rock.png", sound = "death.mp3", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil};
-  bob = {name = "spongebob", image = "bob.jpeg", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil};
+  ['obunga'] = {image = "obunga.png", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil},
+  ['baller'] = {image = "baller.png", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil},
+  ['rock'] = {image = "rock.png", sound = "death.mp3", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil},
+  ['spongebob'] = {image = "bob.jpeg", sound = "", vel = 3, pos = lovr.math.newVec3(0, NB_SIZE / 2, 0), tex = nil, mat = nil}
 }
 
 function lovr.load()
   sky = lovr.graphics.newTexture("sky.jpg", {})
 
-  for i = #nextbots, 1, -1 do
-    nextbots[i].tex = lovr.graphics.newTexture(nextbots[i].image, {})
-    nextbots[i].mat = lovr.graphics.newMaterial(nextbots[i].tex)
-    nextbots[i].pos = lovr.math.newVec3(lovr.math.random(1, 10), NB_SIZE / 2, lovr.math.random(1, 10))
+  for k,v in pairs(nextbots) do
+    v.tex = lovr.graphics.newTexture(v.image, {})
+    v.mat = lovr.graphics.newMaterial(v.tex)
+    v.pos = lovr.math.newVec3(lovr.math.random(-RANDOM_POS_MAX, RANDOM_POS_MAX), NB_SIZE / 2, lovr.math.random(-RANDOM_POS_MAX, RANDOM_POS_MAX))
   end
 end
 
@@ -29,20 +31,20 @@ function lovr.update(dt)
   headset_pos = lovr.math.newVec3(lovr.headset.getPosition())
   headset_angle, headset_ax, headset_ay, headset_az = lovr.headset.getOrientation()
 
-  for i = #nextbots, 1, -1 do
-    local distance = nextbots[i].pos:distance(headset_pos)
-    local nb_pos_tmp = nextbots[i].pos:lerp(headset_pos, (nextbots[i].vel / distance) * dt)
-    nextbots[i].pos = lovr.math.newVec3(nb_pos_tmp.x, NB_SIZE / 2, nb_pos_tmp.z)
+  for k,v in pairs(nextbots) do
+    local distance = v.pos:distance(headset_pos)
+    local nb_pos_tmp = v.pos:lerp(headset_pos, (v.vel / distance) * dt)
+    v.pos = lovr.math.newVec3(nb_pos_tmp.x, NB_SIZE / 2, nb_pos_tmp.z)
   end
 end
 
 function lovr.draw()
   lovr.graphics.skybox(sky)
 
-  for i = #nextbots, 1, -1 do
-    local x, y, z = nextbots[i].pos:unpack()
-    lovr.graphics.plane(nextbots[i].mat, x, y, z, 2.5, 2.5, headset_angle, headset_ax, headset_ay, headset_az) -- next bot image / body
+  for k,v in pairs(nextbots) do
+    local x, y, z = v.pos:unpack()
+    lovr.graphics.plane(v.mat, x, y, z, 2.5, 2.5, headset_angle, headset_ax, headset_ay, headset_az) -- next bot image / body
   end
 
-  lovr.graphics.plane('fill', 0, 0, 0, 20, 20, math.rad(90), 1, 0, 0) -- floor
+  lovr.graphics.plane('fill', 0, 0, 0, 40, 40, math.rad(90), 1, 0, 0) -- floor
 end
